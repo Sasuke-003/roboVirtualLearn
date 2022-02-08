f7d4f2265dc1a7f36fc947b654016930b183501f
B L Nithyashree <blnithyashree@NithyashreeBL-MBP.local>
2022-02-08 15:19:27 +0530

    Added screens

---
+++ src/screens/VIR_MyCourses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const VIR_SearchScreen = () => {
+  return (
+    <View>
+      <Text>My Courses</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default VIR_SearchScreen;
